d390d6a0f847ebca56d5c11cc94604f640f392dd
Jewel Donkor Apeko <49363620+apekodonkorjewel@users.noreply.github.com>
2022-09-18 00:55:52 -0400

    Challenge 4 completed

---
+++ M4-Challenge-Apeko-Jewel/src/main/java/com/example/utilityapi/controller/CalculatorController.java
@@ -0,0 +1,26 @@
+package com.example.utilityapi.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class CalculatorController {
+
+    public CalculatorController() {
+    }
+
+    @RequestMapping(value = "/calculator/divide", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public double divide(@RequestParam int value1, @RequestParam int value2) {
+        if(value2 == 0)
+            throw new ArithmeticException("Illegal attempt to divide by zero");
+        return value1 / value2;
+    }
+
+    @RequestMapping(value = "/calculator/square/{value}", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public int square(@PathVariable int value) {
+        Math.multiplyExact(value,value);
+        return value * value;
+    }
+}
