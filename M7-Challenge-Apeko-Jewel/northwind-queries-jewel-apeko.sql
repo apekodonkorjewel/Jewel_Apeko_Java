fe5d4b4f3a0e20b9819bd03c1e1305ead7d72bf4
Jewel Donkor Apeko <49363620+apekodonkorjewel@users.noreply.github.com>
2022-10-04 15:21:59 -0400

    Challenge 7 completed

---
+++ M7-Challenge-Apeko-Jewel/northwind-queries-jewel-apeko.sql
@@ -0,0 +1,49 @@
+--What are the categories of products in the database?
+select distinct category 
+from northwind.products p;
+
+--What products are made by Dell?
+select * 
+from northwind.products p 
+where product_name like 'Dell%';
+
+--List all the orders shipped to Pennsylvania.
+select * 
+from northwind.orders o 
+where ship_state = 'Pennsylvania';
+
+--List the first name and last name of all employees 
+--with last names that start with the letter W.
+select first_name, last_name  
+from northwind.employees e
+where last_name like 'W%';
+
+--List all customers from zip codes that start with 55.
+select * 
+from northwind.customers c
+where postal_code like '55%';
+
+--List all customers from zip codes that end with 0.
+select * 
+from northwind.customers c
+where postal_code like '%0';
+
+--List the first name, last name, and email for all 
+--customers with a ".org" email address.
+select first_name ,last_name, phone
+from northwind.customers c
+where email like '%.org';
+
+--List the first name, last name, and phone number for 
+--all customers from the 202 area code.
+select first_name, last_name, phone
+from northwind.customers c
+where phone like '%(202)%';
+
+--List the first name, last name, and phone number for 
+--all customers from the 202 area code, ordered 
+--by last name, first name.
+select first_name, last_name, phone
+from northwind.customers c
+where phone like '%(202)%'
+order by last_name, first_name;
